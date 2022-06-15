deda2caa8879c5fb5bda6170ba145140a7faab24
Lucas Lima <lucas.lldcosta@gmail.com>
2022-06-15 19:18:18 -0300

    [feature] add reanimated example

--- exmaple/anchor-point-test/App.tsx
+++ exmaple/anchor-point-test/App.tsx
@@ -1,12 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import AnchorPointDemo from './AnchorPointDemo';
+import React, { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import AnchorPointDemo from "./AnchorPointDemo";
+import ReanimatedDemo from "./ReanimatedDemo";
 
 export default function App() {
+  const [selected, setSelected] = useState(0);
   return (
     <View style={styles.container}>
-      <AnchorPointDemo/>
+      {selected === 0 ? <AnchorPointDemo /> : <ReanimatedDemo />}
+      <View style={styles.tabBar}>
+        <TouchableOpacity onPress={() => setSelected(0)} style={styles.tabBarItem}>
+          <Text>Animated Ex </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setSelected(1)} style={styles.tabBarItem}>
+          <Text>Reanimated 2 Ex </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -14,8 +23,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  tabBar: {
+    height: 50,
+    width: "90%",
+    position: "absolute",
+    backgroundColor: "#ddd",
+    bottom: 40,
+    alignSelf: "center",
+    flexDirection: "row",
+    borderRadius: 5,
+  },
+  tabBarItem: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
